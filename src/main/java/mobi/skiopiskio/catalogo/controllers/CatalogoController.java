4517779998876a8d8a861b30e5222d67d847f442
andrea.kopytko <andrea.kopytko@marconirovereto.it>
2024-05-26 21:16:11 +0200

    added-productDetail

--- src/main/java/mobi/skiopiskio/catalogo/controllers/CatalogoController.java
+++ src/main/java/mobi/skiopiskio/catalogo/controllers/CatalogoController.java
@@ -1,12 +1,13 @@
 package mobi.skiopiskio.catalogo.controllers;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import mobi.skiopiskio.catalogo.domains.Prodotto;
@@ -26,7 +27,19 @@ public class CatalogoController {
 
     @GetMapping("catalogo/nuovo")
     public ModelAndView catalogoForm() {
-        return new ModelAndView("catalogoForm");
+        return new ModelAndView("catalogoForm").addObject(new Prodotto());
+    }
+
+    @PostMapping("/new")
+    public ModelAndView newProductHandler(@ModelAttribute Prodotto prodotto) {
+        service.addProdotto(prodotto);
+        return new ModelAndView("homepage");
+    }
+
+    @GetMapping(params = "code")
+    public ModelAndView productInfo(@RequestParam("code") String code) {
+        Prodotto chosenProduct = service.getProdottoByCode(code);
+        return new ModelAndView("productInfo").addObject("product", chosenProduct);
     }
 
 }
--- src/main/java/mobi/skiopiskio/catalogo/services/CatalogoService.java
+++ src/main/java/mobi/skiopiskio/catalogo/services/CatalogoService.java
@@ -3,8 +3,11 @@ package mobi.skiopiskio.catalogo.services;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import mobi.skiopiskio.catalogo.domains.Prodotto;
 
+@Service
 public class CatalogoService {
 
     private ArrayList<Prodotto> prodotti = new ArrayList<>();
@@ -21,16 +24,16 @@ public class CatalogoService {
         prodotti.add(prodotto);
     }
 
-    public Optional<Prodotto> getProdottoByCode(String code) {
+    public Prodotto getProdottoByCode(String code) {
 
         for(Prodotto p : prodotti) {
 
             if(p.getCode().equals(code)) {
-                return Optional.of(p);
+                return p;
             }
         }
 
-        return Optional.empty();
+        return null;
     }
 
     /* 
@@ -38,9 +41,10 @@ public class CatalogoService {
     Prodotto prod2 = new Prodotto("2", "Erba finta", "Strumento musicale", 2025, 24);
     Prodotto prod3 = new Prodotto("3", "Birmarzio", "Fertilizzante", 2018, 90);
 
-    prodotti.addProdotto(prod1);
-    prodotti.addProdotto(prod2);
-    prodotti.addProdotto(prod3);
+    prodotti.add(prod1);
+    prodotti.add(prod2);
+    prodotti.add(prod3);
 
-    */
+     */
+
 }
